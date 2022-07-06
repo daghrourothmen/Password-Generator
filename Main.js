00388bb1ddffc98272cecc35bfad3ca97b9e782d
daghrour@outlook.de <daghrourothmen>
2022-07-06 20:39:39 +0200

    Initializing the project

---
+++ Main.js
@@ -0,0 +1,20 @@
+const AlphabetTableUppercase=['A','B','C','D','E','F',
+                     'G','H','I','J','K','L',
+                     'M','N','O','P','Q','R',
+                     'S','T','U','V','W','X',
+                     'Y','Z'];
+
+const AlphabetTableLowerCase=AlphabetTableUppercase.map((Upper)=>{
+ return Upper.toLocaleLowerCase();
+});
+
+const NumbersTable=[0,1,2,3,4,5,6,7,8,9];
+
+const SymbolTable=['.',',','?','§','!','<','>','=','*','/','-','+','|','^','(',')','_','%','°','{','}','&'];
+
+function PasswordGenerator(){
+
+};
+
+console.log(AlphabetTableLowerCase);
+console.log(AlphabetTableUppercase);
