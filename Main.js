a99ed2a36f2f336abc3ee9e2fd4fa6772b4c3457
daghrour@outlook.de <daghrourothmen>
2022-07-06 20:43:42 +0200

    Adding random argument

--- Main.js
+++ Main.js
@@ -12,9 +12,13 @@ const NumbersTable=[0,1,2,3,4,5,6,7,8,9];
 
 const SymbolTable=['.',',','?','§','!','<','>','=','*','/','-','+','|','^','(',')','_','%','°','{','}','&'];
 
+const RandomTable=((Math.random())*10).toFixed(0);
+
 function PasswordGenerator(){
 
 };
 
-console.log(AlphabetTableLowerCase);
-console.log(AlphabetTableUppercase);
+// console.log(AlphabetTableLowerCase);
+// console.log(AlphabetTableUppercase);
+
+console.log(RandomTable);
