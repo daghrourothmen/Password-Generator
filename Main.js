ec16d5cc7ca4052ef009c08a57dd83ef0a63c4f2
daghrour@outlook.de <daghrourothmen>
2022-07-09 12:10:14 +0200

    Enhanced password strength

--- Main.js
+++ Main.js
@@ -14,17 +14,38 @@ const SymbolTable=['.',',','?','§','!','<','>','=','*','/','-','+','|','^','(',
 
 const RandomTable=((Math.random())*10).toFixed(0);
 
+const TableName=['AlphabetTableUppercase','AlphabetTableLowerCase','NumbersTable','SymbolTable'];
+
 let length=8;
 let Password='';
-let RandomPosition=((Math.random())*10).toFixed();
+let RandomPosition=((Math.random())*10).toFixed(0);
+let RandomT1=((Math.random())*10).toFixed(0);
+let RandomT2=((Math.random())*10).toFixed(0);
+let RandomT3=((Math.random())*10).toFixed(0);
+let RandomT4=((Math.random())*10).toFixed(0);
+
+function ControlledRandomNumbers(){
+
+    while(RandomT1>3 || RandomT2>3 || RandomT3>3 || RandomT4>3){
+        RandomT1=((Math.random())*10).toFixed(0);
+        RandomT2=((Math.random())*10).toFixed(0);
+        RandomT3=((Math.random())*10).toFixed(0);
+        RandomT4=((Math.random())*10).toFixed(0);
+    }
+
+    return [RandomT1,RandomT2,RandomT3,RandomT4];
+
+};
 
 function PasswordGenerator(){
 
     while(length >0)
     {
 
-        Password=Password+SymbolTable[RandomPosition]+NumbersTable[RandomPosition]
-                         +AlphabetTableLowerCase[RandomPosition]+AlphabetTableUppercase[RandomPosition];
+        Password=Password+TableName[ControlledRandomNumbers()[0]][RandomPosition]+
+                          TableName[ControlledRandomNumbers()[1]][RandomPosition]+
+                          TableName[ControlledRandomNumbers()[2]][RandomPosition]+
+                          TableName[ControlledRandomNumbers()[3]][RandomPosition];
         RandomPosition=((Math.random())*10).toFixed();
         length--;
 
@@ -34,7 +55,6 @@ function PasswordGenerator(){
 
 };
 
-// console.log(AlphabetTableLowerCase);
-// console.log(AlphabetTableUppercase);
 
 console.log(PasswordGenerator());
+console.log(ControlledRandomNumbers());
